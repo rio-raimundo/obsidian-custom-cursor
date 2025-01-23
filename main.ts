1d8fea4e8c18f931ccd6f4420a21781814c78972
bitesizing <r.raimundo2001@gmail.com>
2025-01-23 20:30:52 +0100

    , added blinkCursor architecture to js

--- main.ts
+++ main.ts
@@ -5,14 +5,26 @@ type MarkdownSubView = { sizerEl: HTMLElement };
 export default class SmoothTypingAnimation extends Plugin {
 	lastPos: Coordinates = { left: 0, top: 0};
 	cursorElement: HTMLSpanElement;
+	blinkStartTime: number = Date.now();
 
 	// Contains the architecture which will be called when the main function needs to return
 	private scheduleNextUpdate() {
 		requestAnimationFrame(this.updateCursor.bind(this));
 	}
 
+	private blinkCursor() {
+		// Return an opacity of 1 for the first 'half' of the blink, then an opacity of 0 for the second half
+		const timePassed = Date.now() - this.blinkStartTime;
+		if (timePassed < 250) { return 1; }
+		else if (timePassed < 500) { return 0; }
+
+		// Once blink has been processed, reset the timer and return 1
+		requestAnimationFrame(() => { this.blinkStartTime = Date.now(); });
+		return 1;
+	}
+
 	// Main function, called every frame
-	async updateCursor() {
+	updateCursor() {
 		// Get reference to selection
 		const parentElement = (<MarkdownSubView>(
 			(<unknown>(
@@ -54,7 +66,8 @@ export default class SmoothTypingAnimation extends Plugin {
 		this.cursorElement = document.body.createSpan({
 			cls: "custom-cursor",
 		});
-
+		
+		//  Create listeners to see if the cursor is currently on the right page (disappears if not)
 		window.addEventListener('blur', () => {
 			document.body.classList.add('window-blurred');
 		});
@@ -63,7 +76,8 @@ export default class SmoothTypingAnimation extends Plugin {
 			document.body.classList.remove('window-blurred');
 		});
 
-		// Call our initial function, which will recall itself.
-		this.updateCursor();
+		// Initialise variables and schedule our first function call, which will be recalled once per frame.
+		requestAnimationFrame(() => { this.blinkStartTime = Date.now(); });
+		this.scheduleNextUpdate();
 	}
 }
