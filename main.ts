edeac60aa94c26708d79416cab976cdd768ee24e
bitesizing <r.raimundo2001@gmail.com>
2025-01-23 21:57:21 +0100

    . Started implementing smooth typing (not currently working)

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 import { Plugin, MarkdownView } from "obsidian";
 type Coordinates = { left: number; top: number};
-type MarkdownSubView = { sizerEl: HTMLElement };
+type Position = { line: number; ch: number };
 
 export default class SmoothTypingAnimation extends Plugin {
-	lastPos: Coordinates = { left: 0, top: 0};
 	cursorElement: HTMLSpanElement;
+	prevCursorCoordinates: Coordinates = { left: 0, top: 0};
+	prevCursorPosition: Position = { line: 0, ch: 0 };
+	cursorMovePositionStartTime: number = Date.now();
 	blinkStartTime: number = Date.now();
+	blinkDuration = 1200;
 
 	// Contains the architecture which will be called when the main function needs to return
 	private scheduleNextUpdate() {
@@ -24,30 +27,36 @@ export default class SmoothTypingAnimation extends Plugin {
 
 		// Return an opacity of 1 for the first 'half' of the blink, then an opacity of 0 for the second half
 		const timePassed = Date.now() - this.blinkStartTime;
-		if (timePassed < 500) { return 1; }
-		else if (timePassed < 1000) { return 0; }
+		if (timePassed < this.blinkDuration/2) { return 1; }
+		else if (timePassed < this.blinkDuration) { return 0; }
 
 		// Once blink has been processed, reset the timer and return 1
 		return this.resetCursor();
 	}
 
-	private returnReferences() {
-		// Get reference to selection
-		const parentElement = (<MarkdownSubView>(
-			(<unknown>(
-				this.app?.workspace.getActiveViewOfType(MarkdownView)
-					?.currentMode
-			))
-		))?.sizerEl?.parentElement;
-		const selection = activeWindow.getSelection();
+	private handleSmoothTyping(activeLeaf: MarkdownView | null) {
+		if (!activeLeaf) { return; }
 
-		return { selection };
+		const currentCursorPosition: Position = activeLeaf.editor.getCursor();
+		
+		// Handle no movement case 
+		const charIncremented = Math.abs(this.prevCursorPosition.ch - currentCursorPosition.ch) === 1;
+		if (this.prevCursorPosition.line === currentCursorPosition.line && this.prevCursorPosition.ch === currentCursorPosition.ch) { return; }
+		const goodMovement = (this.prevCursorPosition.line === currentCursorPosition.line && charIncremented);
+
+
+	} 
+
+	private returnReferences(): { selection: Selection | null; activeLeaf: MarkdownView | null } {
+		const selection = activeWindow.getSelection();
+		const activeLeaf = this.app.workspace.getActiveViewOfType(MarkdownView); 
+		return { selection, activeLeaf };
 	}
 
 	// Main function, called every frame
 	updateCursor() {
-		// Return if selection does not exist
-		const { selection } = this.returnReferences();
+		// Get needed references and return if there is no selection
+		const { selection, activeLeaf } = this.returnReferences();
 		if (!selection || !selection.focusNode) {
 			this.scheduleNextUpdate();
 			return;
@@ -59,10 +68,10 @@ export default class SmoothTypingAnimation extends Plugin {
 		const cursorDetails = cursorRange.getBoundingClientRect();
 
 		// Check if cursor position has changed
-		const cursorPositionChanged = (this.lastPos.left !== cursorDetails.left || this.lastPos.top !== cursorDetails.top);
+		const cursorCoordinatesChanged = (this.prevCursorCoordinates.left !== cursorDetails.left || this.prevCursorCoordinates.top !== cursorDetails.top);
 
 		// Calculate current cursor opacity 
-		const blinkOpacity = this.blinkCursor(cursorPositionChanged);
+		const blinkOpacity = this.blinkCursor(cursorCoordinatesChanged);
 
 		// Send cursor details to .css to render
 		this.cursorElement.style.setProperty("--cursor-x1", `${cursorDetails.left}px`);
@@ -71,7 +80,7 @@ export default class SmoothTypingAnimation extends Plugin {
 		this.cursorElement.style.setProperty("--cursor-opacity", `${blinkOpacity}`);
 
 		//  Update this.lastPos and recall
-		this.lastPos = {
+		this.prevCursorCoordinates = {
 			left: cursorDetails.left,
 			top: cursorDetails.top,
 		}
@@ -98,5 +107,11 @@ export default class SmoothTypingAnimation extends Plugin {
 		// Initialise variables and schedule our first function call, which will be recalled once per frame.
 		requestAnimationFrame(() => { this.blinkStartTime = Date.now(); });
 		this.scheduleNextUpdate();
+
+		this.registerEvent(
+			this.app.workspace.on('editor-change', (editor, changes) => {
+				console.log(changes)
+			})
+		);
 	}
 }
