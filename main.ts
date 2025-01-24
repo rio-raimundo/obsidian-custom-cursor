51c145b38b543148ae0789cbc72dfb65009ef981
bitesizing <r.raimundo2001@gmail.com>
2025-01-24 18:54:49 +0100

    , First implementation of smooth typing handling!

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
+import { get } from "http";
 import { Plugin, MarkdownView } from "obsidian";
 type Coordinates = { left: number; top: number};
 type Position = { line: number; ch: number };
 
 export default class SmoothTypingAnimation extends Plugin {
 	cursorElement: HTMLSpanElement;
-	prevCursorCoordinates: Coordinates = { left: 0, top: 0};
-	prevCursorPosition: Position = { line: 0, ch: 0 };
-	cursorMovePositionStartTime: number = Date.now();
+
+	prevCursorCoords: Coordinates = { left: 0, top: 0};  // measured in px
+	prevCursorPos: Position | null = { line: 0, ch: 0 };  // measured in line and character
+	prevIconCoords: Coordinates | null = { left: 0, top: 0 };  // coordinates of the visible 'icon' (not cursor itself)
+	
+	prevFrameTime: number = Date.now();
 	blinkStartTime: number = Date.now();
 	blinkDuration = 1200;
+
+	characterMovementTime = 80;
+	remainingMoveTime = 0;
 
 	// Contains the architecture which will be called when the main function needs to return
 	private scheduleNextUpdate() {
@@ -21,9 +28,9 @@ export default class SmoothTypingAnimation extends Plugin {
 		return 1;
 	}
 
-	private blinkCursor(cursorPositionChanged: boolean): number {
+	private blinkCursor(cursorPosChanged: boolean): number {
 		// Check if cursor position has changed
-		if (cursorPositionChanged) { return this.resetCursor(); }
+		if (cursorPosChanged) { return this.resetCursor(); }
 
 		// Return an opacity of 1 for the first 'half' of the blink, then an opacity of 0 for the second half
 		const timePassed = Date.now() - this.blinkStartTime;
@@ -34,17 +41,40 @@ export default class SmoothTypingAnimation extends Plugin {
 		return this.resetCursor();
 	}
 
-	private handleSmoothTyping(activeLeaf: MarkdownView | null) {
-		if (!activeLeaf) { return; }
+	// Handles smooth typing, and returns fraction of distance to travel this frame
+	private handleSmoothTyping(currCursorPos: Position | null, timeSinceLastFrame: number): number {
+		const returnStatement = (fractionTravelled = 0) => {
+			this.remainingMoveTime = 0;
+			this.prevCursorPos = currCursorPos;
+			return fractionTravelled;
+		}
 
-		const currentCursorPosition: Position = activeLeaf.editor.getCursor();
+		// If current cursor position does not exist
+		if (!currCursorPos || !this.prevCursorPos) { return returnStatement(); }
 		
-		// Handle no movement case 
-		const charIncremented = Math.abs(this.prevCursorPosition.ch - currentCursorPosition.ch) === 1;
-		if (this.prevCursorPosition.line === currentCursorPosition.line && this.prevCursorPosition.ch === currentCursorPosition.ch) { return; }
-		const goodMovement = (this.prevCursorPosition.line === currentCursorPosition.line && charIncremented);
+		const charIncremented = Math.abs(this.prevCursorPos.ch - currCursorPos.ch) === 1;
+		const charMoved = Math.abs(this.prevCursorPos.ch - currCursorPos.ch) !== 0;
+		const lineMoved = Math.abs(this.prevCursorPos.line - currCursorPos.line) !== 0;
 
+		// If there has been a sharpMovement of the true cursor, we cancel the smooth movement of the icon
+		if ((charMoved && !charIncremented) || (lineMoved)) {
+			this.remainingMoveTime = 0;
+		}
 
+		// If there has been a smoothMovement of the true cursor, we add to the movement time remaining
+		else if (charIncremented && !lineMoved) {
+			console.log('smooth Movement!')
+			this.remainingMoveTime += this.characterMovementTime;
+		}
+		
+		// Regardless of movement, we get the fraction of the total distance travelled (timeSinceLastFrame / remainingMovementTime)
+		// and remove the timeSinceLastFrame from the remainingMovementTime
+		if (this.remainingMoveTime <= 0) { return returnStatement(); }
+		const fractionTravelled = Math.min(timeSinceLastFrame / this.remainingMoveTime, 1);
+		this.remainingMoveTime = Math.max(0, this.remainingMoveTime - timeSinceLastFrame);
+
+		// Update prevCursorPosition
+		return returnStatement(fractionTravelled);
 	} 
 
 	private returnReferences(): { selection: Selection | null; activeLeaf: MarkdownView | null } {
@@ -53,8 +83,17 @@ export default class SmoothTypingAnimation extends Plugin {
 		return { selection, activeLeaf };
 	}
 
+	private getTimeSinceLastFrame(): number {
+		const currentTime = Date.now();
+		const timeSinceLastFrame = currentTime - this.prevFrameTime;
+		return timeSinceLastFrame;
+	}
+
 	// Main function, called every frame
 	updateCursor() {
+		// Keep track of time on each frame, and how much has elapsed
+		const timeSinceLastFrame = this.getTimeSinceLastFrame();
+
 		// Get needed references and return if there is no selection
 		const { selection, activeLeaf } = this.returnReferences();
 		if (!selection || !selection.focusNode) {
@@ -65,24 +104,41 @@ export default class SmoothTypingAnimation extends Plugin {
 		// Take the focused 'node', turn it into a range from start to finish
 		const cursorRange = document.createRange();
 		cursorRange.setStart(selection.focusNode, selection.focusOffset)
-		const cursorDetails = cursorRange.getBoundingClientRect();
+		const currCursorCoords = cursorRange.getBoundingClientRect();
+		const currCursorPos: Position | null = activeLeaf ? activeLeaf.editor.getCursor() : null;
 
 		// Check if cursor position has changed
-		const cursorCoordinatesChanged = (this.prevCursorCoordinates.left !== cursorDetails.left || this.prevCursorCoordinates.top !== cursorDetails.top);
+		const cursorCoordinatesChanged = (this.prevCursorCoords.left !== currCursorCoords.left || this.prevCursorCoords.top !== currCursorCoords.top);
 
 		// Calculate current cursor opacity 
 		const blinkOpacity = this.blinkCursor(cursorCoordinatesChanged);
 
+		// Get the fraction of total distance that the cursor icon should travel this frame
+		// nonzero if currently smoothly moving
+		// and turn it into a true distance
+		const iconMovementFraction = this.handleSmoothTyping(currCursorPos, timeSinceLastFrame);
+		let currIconCoords: Coordinates = {left: currCursorCoords.left, top: currCursorCoords.top};
+		if (iconMovementFraction !== 0)	{			
+			const movementThisFrame: Coordinates = {
+				left: iconMovementFraction * (currCursorCoords.left - this.prevCursorCoords.left),
+				top: iconMovementFraction * (currCursorCoords.top - this.prevCursorCoords.top)
+			};
+			currIconCoords = {
+				left: this.prevCursorCoords.left + movementThisFrame.left,
+				top: this.prevCursorCoords.top + movementThisFrame.top
+			};
+		}
+
 		// Send cursor details to .css to render
-		this.cursorElement.style.setProperty("--cursor-x1", `${cursorDetails.left}px`);
-		this.cursorElement.style.setProperty("--cursor-y1", `${cursorDetails.top}px`);
-		this.cursorElement.style.setProperty("--cursor-height", `${cursorDetails.height}px`);
+		this.cursorElement.style.setProperty("--cursor-x1", `${currIconCoords.left}px`);
+		this.cursorElement.style.setProperty("--cursor-y1", `${currIconCoords.top}px`);
+		this.cursorElement.style.setProperty("--cursor-height", `${currCursorCoords.height}px`);
 		this.cursorElement.style.setProperty("--cursor-opacity", `${blinkOpacity}`);
 
 		//  Update this.lastPos and recall
-		this.prevCursorCoordinates = {
-			left: cursorDetails.left,
-			top: cursorDetails.top,
+		this.prevCursorCoords = {
+			left: currCursorCoords.left,
+			top: currCursorCoords.top,
 		}
 
 		// Schedule next update
@@ -107,11 +163,5 @@ export default class SmoothTypingAnimation extends Plugin {
 		// Initialise variables and schedule our first function call, which will be recalled once per frame.
 		requestAnimationFrame(() => { this.blinkStartTime = Date.now(); });
 		this.scheduleNextUpdate();
-
-		this.registerEvent(
-			this.app.workspace.on('editor-change', (editor, changes) => {
-				console.log(changes)
-			})
-		);
 	}
 }
